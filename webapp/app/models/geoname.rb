c4990a57c4862a5a467ce8d103582e7d169cbee3
Omar Al-Safi <omarsmak@gmail.com>
2014-12-12 02:24:17 +0000

    Cleaned the geo tables and limit them to geoname and geoname_fulltext

---
+++ webapp/app/models/geoname.rb
@@ -0,0 +1,4 @@
+class Geoname < ActiveRecord::Base
+    set_table_name 'geoname'
+    set_primary_key 'geonameid'
+end
---
+++ webapp/app/models/geoname_fulltext.rb
@@ -0,0 +1,4 @@
+class GeonameFulltext < ActiveRecord::Base
+    set_table_name 'geoname_fulltext'
+    set_primary_key 'geonameid'
+end
